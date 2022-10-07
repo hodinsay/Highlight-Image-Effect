daba82f70cba0d393e1ff906d2e3059edadf4130
Din S Ho <dinsho@Din-S-M1.local>
2022-10-07 15:36:57 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const focus = document.getElementById("focus");
+const focusHalfWidth = focus.offsetWidth/2;
+
+document.onmousemove = function(e) {
+    focus.style.left = e.pageX - focusHalfWidth + "px";
+    focus.style.top = e.pageY - focusHalfWidth + "px";
+}
